05725171f78f10995b47f771a55f4f825aff765a
ricejson <wjjbangbangbang@163.com>
2025-04-09 15:42:17 +0800

    新增slice公开包下filter方法

--- slice/map.go
+++ slice/map.go
@@ -8,3 +8,14 @@ func Map[Src any, Dst any](s []Src, f func(Src) Dst) []Dst {
 	}
 	return ret
 }
+
+// Filter 将原切片不符合要求的元素过滤并转换为新切片返回
+func Filter[T any](src []T, matchFunc func(e T) bool) []T {
+	var ret = make([]T, 0, len(src))
+	for _, v := range src {
+		if matchFunc(v) {
+			ret = append(ret, v)
+		}
+	}
+	return ret
+}
--- slice/map_test.go
+++ slice/map_test.go
@@ -3,6 +3,7 @@ package slice
 import (
 	"github.com/go-playground/assert/v2"
 	"strconv"
+	"strings"
 	"testing"
 )
 
@@ -27,6 +28,33 @@ func TestSlice_Map(t *testing.T) {
 		t.Run(tc.name, func(t *testing.T) {
 			res := Map[int, string](tc.src, func(i int) string {
 				return strconv.Itoa(i)
+			})
+			assert.Equal(t, res, tc.wantRes)
+		})
+	}
+}
+
+func TestMap_Filter(t *testing.T) {
+	testCases := []struct {
+		name    string
+		src     []string
+		wantRes []string
+	}{
+		{
+			name:    "切片为空",
+			src:     []string{},
+			wantRes: []string{},
+		},
+		{
+			name:    "切片不为空",
+			src:     []string{"jack", "rocky", "mirror"},
+			wantRes: []string{"rocky"},
+		},
+	}
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			res := Filter[string](tc.src, func(e string) bool {
+				return strings.HasPrefix(e, "r")
 			})
 			assert.Equal(t, res, tc.wantRes)
 		})
