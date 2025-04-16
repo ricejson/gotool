383fcf700bbc9260d125ada8e7f8ce5b5c8fca3a
ricejson <wjjbangbangbang@163.com>
2025-04-16 16:02:07 +0800

    新增公开包下三元运算符函数实现

--- condition_test.go
+++ condition_test.go
@@ -1,6 +1,7 @@
 package gotool
 
 import (
+	"fmt"
 	"github.com/go-playground/assert/v2"
 	"testing"
 )
@@ -32,6 +33,53 @@ func TestCondition_IfThenElse(t *testing.T) {
 		t.Run(tc.name, func(t *testing.T) {
 			res := IfThenElse[string](tc.condition, tc.trueValue, tc.falseValue)
 			assert.Equal(t, res, tc.wantRes)
+		})
+	}
+}
+
+func TestCondition_IfThenElseFunc(t *testing.T) {
+	testCases := []struct {
+		name      string
+		condition bool
+		trueFunc  func() (string, error)
+		falseFunc func() (string, error)
+		wantRes   string
+		wantErr   error
+	}{
+		{
+			name:      "条件成立",
+			condition: true,
+			trueFunc: func() (string, error) {
+				fmt.Println("条件成立")
+				return "true", nil
+			},
+			falseFunc: func() (string, error) {
+				fmt.Println("条件不成立")
+				return "false", nil
+			},
+			wantRes: "true",
+			wantErr: nil,
+		},
+		{
+			name:      "条件不成立",
+			condition: false,
+			trueFunc: func() (string, error) {
+				fmt.Println("条件成立")
+				return "true", nil
+			},
+			falseFunc: func() (string, error) {
+				fmt.Println("条件不成立")
+				return "false", nil
+			},
+			wantRes: "false",
+			wantErr: nil,
+		},
+	}
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			res, err := IfThenElseFunc[string](tc.condition, tc.trueFunc, tc.falseFunc)
+			assert.Equal(t, res, tc.wantRes)
+			assert.Equal(t, err, tc.wantErr)
 		})
 	}
 }
