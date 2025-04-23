49329483ea3ec01b970b1a333543ba3f79057802
ricejson <wjjbangbangbang@163.com>
2025-04-23 23:21:32 +0800

    修改retry公开包下重试策略为线程安全版本

--- retry/fixed_interval.go
+++ retry/fixed_interval.go
@@ -9,7 +9,7 @@ import (
 // FixedIntervalStrategy 固定间隔重试策略
 type FixedIntervalStrategy struct {
 	maxRetries int32         // 最大重试次数（<=0表示无限重试）
-	curRetries *int32        // 当前重试次数
+	curRetries int32         // 当前重试次数
 	interval   time.Duration // 重试间隔
 }
 
@@ -17,22 +17,29 @@ func NewFixedIntervalStrategy(maxRetries int32, interval time.Duration) (*FixedI
 	if interval <= 0 {
 		return nil, errors.New("invalid retry interval")
 	}
-	var curRetries int32 = 0
 	return &FixedIntervalStrategy{
 		interval:   interval,
 		maxRetries: maxRetries,
-		curRetries: &curRetries,
+		curRetries: 0,
 	}, nil
 }
 
 func (s *FixedIntervalStrategy) Next() (time.Duration, bool) {
-	// 判断是否超时最大时间间隔
-	var curRetriesVal = atomic.LoadInt32(s.curRetries)
-	if s.maxRetries <= 0 || curRetriesVal < s.maxRetries {
+	if s.maxRetries <= 0 {
 		// 可以继续重试
-		atomic.AddInt32(s.curRetries, 1)
+		atomic.AddInt32(&s.curRetries, 1)
 		return s.interval, true
 	}
+	// 判断是否超时最大时间间隔
+	var curRetriesVal = atomic.LoadInt32(&s.curRetries)
+	for curRetriesVal < s.maxRetries {
+		if !atomic.CompareAndSwapInt32(&s.curRetries, curRetriesVal, curRetriesVal+1) {
+			curRetriesVal = atomic.LoadInt32(&s.curRetries)
+			continue
+		}
+		return s.interval, true
+	}
+
 	// 超出最大重试次数
 	return 0, false
 }
